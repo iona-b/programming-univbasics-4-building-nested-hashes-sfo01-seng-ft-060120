523e688337a3dc0a79da589eae27b70720e94f78
Iona <ionabrabender@gmail.com>
2020-04-28 02:43:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ hash = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
-hash = {:railroads => {:pieces => 4, rent_in_dollars => {}, :names => {}}}
+hash = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
 end
 
 def monopoly_with_third_tier
